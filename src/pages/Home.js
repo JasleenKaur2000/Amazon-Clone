84469c5628dd40193817e4ced4808f4c21ce092f
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2021-08-28 00:57:08 +0530

    structured home.js , added headeritems and slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
 import React from "react";
+import "../pages/Home.css";
+import { headerItems } from "../utils/productData";
+import Banner1 from "../banner images/Banner1.PNG";
+import Banner2 from "../banner images/Banner2.PNG";
+import Banner3 from "../banner images/Banner3.PNG";
+import Banner4 from "../banner images/Banner4.PNG";
+import Banner5 from "../banner images/Banner5.PNG";
+import Banner6 from "../banner images/Banner6.PNG";
+import Slider from "../components/Slider";
 
 const Home = () => {
-  return <div className="home">Hello World Again!!</div>;
+  const bannerImages = [Banner1, Banner2, Banner3, Banner4, Banner5, Banner6];
+
+  return (
+    <>
+      <div className="item-container">
+        {headerItems && headerItems.map((item, index) => <p>{item}</p>)}
+      </div>
+      <div className="home">
+        <div className="home-container">
+          <Slider images={bannerImages} />
+          <div className="home-row">{/*products*/}</div>
+          <div style={{ marginTop: "40px" }}>{/*back to top */}</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
